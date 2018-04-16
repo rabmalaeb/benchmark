4790bb040a1d154d916afb6da42023b9025e36ed
Rabih Malaeb <rabmalaeb@gmail.com>
2018-04-16 23:59:25 +0300

    added pager basic functionality

--- src/app/app-pager/app-pager.component.ts
+++ src/app/app-pager/app-pager.component.ts
@@ -12,18 +12,39 @@ import { Employee } from '../employee';
 export class AppPagerComponent implements OnInit {
 
   @Input()
-  employeeId: number;
-  employees: Employee[];
+  employees: Employee[] = [];
+  previousId: number; 
+  itemsPerPage: number = 10;
   constructor(private employeeService: EmployeeService) { }
 
   ngOnInit() {
-    this.getNextEmployees();
+    this.getEmployees();
   }
 
-  getNextEmployees() {
-   this.employeeService.getNextEmployees(this.employeeId)
+  getEmployees() {
+   this.employeeService.getEmployees()
+   .toPromise()
+   .then ((employees) => {
+      employees.map((employee)=> {
+        if(this.employees.length < this.itemsPerPage) {
+        this.employees.push(employee);
+        }
+     }) 
+    })
+  }
+
+  getNextPage() {
+   this.previousId = this.employees[0].employeeId;
+   let lastEmployee = this.employees[this.employees.length-1];
+   this.employeeService.getNextEmployees(lastEmployee.employeeId)
    .toPromise()
    .then (employees => this.employees = employees)
   }
-  
+
+  getPreviousPage() {
+    this.employeeService.getNextEmployees(this.previousId - 1)
+    .toPromise()
+    .then (employees => this.employees = employees)
+   }
+
 }
